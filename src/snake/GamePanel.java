973b610a2ffeb25771cc4a5a53d75344278775cc
Alexdevsoft <alexhavilla2022@gmail.com>
2022-12-05 22:48:34 -0300

    Criando a tela

--- src/snake/GamePanel.java
+++ src/snake/GamePanel.java
@@ -1,13 +1,83 @@
 package snake;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener{
+	private static final long serialVersionUID = 1L;
+	
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	static final int UNIT_SIZE = 25;
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int DELAY = 75;
+	final int x[] = new int[GAME_UNITS];
+	final int y[] = new int[GAME_UNITS];
+	int bodyParts = 6;
+	int applesEaten;
+	int appleX;
+	int appleY;
+	char direction = 'R';
+	boolean running = false;
+	Timer timer;
+	Random random;
 
 	GamePanel(){
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+	}
+	
+	public void startGame() {
+		newApple();
+		running = true;
+		timer = new Timer(DELAY,this);
+		timer.start();
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
+	}
+	
+	public void draw(Graphics g) {
+		for(int i=0; i<SCREEN_HEIGHT/UNIT_SIZE; i++) {
+			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+		}
+		
+	}
+	
+	public void newApple() {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkApple() {
+		
+	}
+	
+	public void checkCollisions() {
+		
+	}
+	
+	public void gameOver(Graphics g) {
 		
 	}
 	
@@ -15,6 +85,13 @@ public class GamePanel extends JPanel implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		
 		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
 	}
 
 }
--- src/snake/SnakeGame.java
+++ src/snake/SnakeGame.java
@@ -4,6 +4,7 @@ public class SnakeGame {
 
 	public static void main(String[] args) {
 		
+		GameFrame frame = new GameFrame();
 
 	}
 
