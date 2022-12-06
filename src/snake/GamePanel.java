3a49243aa2bdd63ca4ca66964dc841380ae8b450
Alexdevsoft <alexhavilla2022@gmail.com>
2022-12-05 22:30:59 -0300

    Jogo da cobra

---
+++ src/snake/GamePanel.java
@@ -0,0 +1,20 @@
+package snake;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+	GamePanel(){
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+
+}
---
+++ src/snake/SnakeGame.java
@@ -0,0 +1,10 @@
+package snake;
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
